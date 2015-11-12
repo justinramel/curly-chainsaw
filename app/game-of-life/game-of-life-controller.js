cb958c61c11a4fdff918e6fd982208979dddba54
Justin Ramel <justin.ramel@gmail.com>
2015-11-12 14:31:33 +0000

    working as directive

--- app/game-of-life/game-of-life-controller.js
+++ app/game-of-life/game-of-life-controller.js
@@ -4,27 +4,66 @@ angular.module('MyApp')
     $stateProvider
       .state('gameOfLife', {
         url: '/game-of-life',
-        templateUrl: 'game-of-life/game-of-life.html',
-        controller: 'GameOfLifeController'
+        template: '<game-of-life></game-of-life>',
       });
   }])
-  .controller('GameOfLifeController', ['$scope', '$interval', function ($scope, $interval) {
+  .service('gameOfLifeService', function () {
     'use strict';
-    $scope.gol = new SAMURAIPRINCIPLE.GameOfLife();
-
-    $scope.toggle = function(event) {
-      var row = Math.floor(event.offsetY / 20);
-      var col = Math.floor(event.offsetX / 20);
-      $scope.gol.toggleCellState(row, col);
-      console.log(event.offsetY, event.offsetX);
-      console.log(row, col);
-      console.log(event);
+    var self = this,
+  		isAlive = {},
+  		cellKey = function (row, column) {
+  			return row + '_' + column;
+  		};
+  	this.isAlive = isAlive;
+  	this.isCellAlive = function (row, column) {
+  		return isAlive[cellKey(row, column)] || false;
+  	};
+  	this.toggleCellState = function (row, column) {
+  		var key = cellKey(row, column);
+  		if (isAlive[key]) {
+  			delete isAlive[key];
+  		} else {
+  			isAlive[key] = {
+  				row: row,
+  				column: column,
+  				isAlive: true
+  			};
+  		}
+  		return this;
+  	};
+  	this.tick = function () {
+  		var key, parts, row, column, numberOfNeighbours = {}, neighbourKey;
+  		for (key in isAlive) {
+  			parts = key.split('_');
+  			row = parseInt(parts[0], 10);
+  			column = parseInt(parts[1], 10);
+  			numberOfNeighbours[key] = numberOfNeighbours[key] || 0;
+  			[[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]].forEach(function (offset) {
+  				neighbourKey = cellKey(row + offset[0], column + offset[1]);
+  				numberOfNeighbours[neighbourKey] = (numberOfNeighbours[neighbourKey] || 0) + 1;
+  			});
+  		}
+  		for (key in numberOfNeighbours) {
+  			if (isAlive[key] && (numberOfNeighbours[key] < 2 || numberOfNeighbours[key] > 3) || !isAlive[key] && numberOfNeighbours[key] === 3) {
+  				parts = key.split('_');
+  				row = parseInt(parts[0], 10);
+  				column = parseInt(parts[1], 10);
+  				self.toggleCellState(row, column);
+  			}
+  		}
+  	};
+  })
+  .directive('gameOfLife', function() {
+    'use strict';
+    return {
+      templateUrl: 'game-of-life/game-of-life.html',
+      controller: function ($scope, gameOfLifeService) {
+        $scope.gol = gameOfLifeService;
+        $scope.toggle = function(event) {
+          var row = Math.floor(event.offsetY / 20);
+          var col = Math.floor(event.offsetX / 20);
+          $scope.gol.toggleCellState(row, col);
+        };
+      }
     };
-
-    $scope.autoTick = function() {
-      $interval(function() {
-        $scope.gol.tick();
-        console.log('tick');
-      }, 300);
-    }
-  }]);
+  });
